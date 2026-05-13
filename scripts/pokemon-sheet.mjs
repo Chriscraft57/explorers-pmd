03d906babeac79ca7a29fdafb6879cc405b01e19
Chris <123362394+Chriscraft57@users.noreply.github.com>
2026-05-13 17:59:30 +0200

    character sheet adjustments

--- scripts/pokemon-sheet.mjs
+++ scripts/pokemon-sheet.mjs
@@ -16,4 +16,20 @@ export class PokemonSheet extends ActorSheet {
 
     return context;
   }
+
+  activateListeners(html) {
+    super.activateListeners(html);
+    html.find('.file-picker').click(this._onBrowseImage.bind(this));
+  }
+
+  _onBrowseImage(event) {
+    event.preventDefault();
+    const input = event.currentTarget.closest('.pmd-image-picker').querySelector('input[name="img"]');
+    if (!input) return;
+    return FilePicker.browse('image', input.value or 'icons/svg/mystery-man.svg', (path) => {
+      input.value = path;
+      input.dispatchEvent(new Event('input', { bubbles: true }));
+      input.dispatchEvent(new Event('change', { bubbles: true }));
+    });
+  }
 }
